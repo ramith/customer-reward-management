8b7facf7be22a3eb3be9dbca7605f52e12b436bd
RolandHewage <roland.hewage@gmail.com>
2023-10-26 17:36:59 +0530

    Integrate data store with loyalty management api

--- loyalty-engine/main.go
+++ loyalty-engine/main.go
@@ -54,8 +54,6 @@ type UserReward struct {
 }
 
 var logger *zap.Logger
-var userRewards []UserReward
-var rewardOffers []RewardOffer
 
 var clientId = os.Getenv("CLIENT_ID")
 var clientSecret = os.Getenv("CLIENT_SECRET")
@@ -84,14 +82,6 @@ func main() {
 
 	r := mux.NewRouter()
 
-	rewardOffers = append(rewardOffers, RewardOffer{"RWD34589", "Target", 25, 500, "A Target GiftCard is your opportunity to shop for thousands of items at more than 1,900 Target stores in the U.S., as well as Target.com. From home décor, small appliances and electronics to fashion, accessories and music, find exactly what you’re looking for at Target. No fees. No expiration. No kidding.™", "https://drive.google.com/file/d/1FEOGLEG99HsttPBXliXUi8aWYqnNmPH2/view?usp=drive_link"})
-	rewardOffers = append(rewardOffers, RewardOffer{"RWD34590", "Starbucks Coffee", 15, 200, "Enjoy a PM pick-me-up with a lunch sandwich, protein box or a bag of coffee—including Starbucks VIA Instant", "https://drive.google.com/file/d/1nku2n63zXBfrA3Bf0eAVWqu45mFLnaRE/view?usp=drive_link"})
-	rewardOffers = append(rewardOffers, RewardOffer{"RWD34591", "Jumba Juice", 6, 600, "Let Jamba come to you – wherever you are. Get our Whirld Famous smoothies, juices, and bowls delivered in just a few clicks. My Jamba rewards members can also apply rewards & earn points on delivery orders when you order on jamba.com or the jamba app!", "https://drive.google.com/file/d/1khJX-N7N8xHrV5o9GvqsH7wApDoY8ej0/view?usp=drive_link"})
-	rewardOffers = append(rewardOffers, RewardOffer{"RWD34592", "Grubhub", 10, 500, "Grubhub offers quick, easy food delivery, either online or through a mobile app. Customers can select from any local participating restaurant. They can add whatever they like to their order and have it delivered right to their home or office by one of Grubhub's delivery drivers. You can save even more by using a Grubhub promo code on your order", "https://drive.google.com/file/d/14S6olzLfOQJatEr4FkXyB_m1l31H2XyJ/view?usp=drive_link"})
-
-	userRewards = append(userRewards, UserReward{"U451298", "RWD34589", "2023-09-04T14:32:21Z", true})
-	userRewards = append(userRewards, UserReward{"U451299", "RWD34590", "2023-09-04T14:32:21Z", true})
-
 	r.HandleFunc("/rewards", getRewardOffers).Methods("GET")
 	r.HandleFunc("/rewards/{id}", getRewardOffer).Methods("GET")
 	r.HandleFunc("/user-rewards", getUserRewards).Methods("GET")
@@ -102,12 +92,32 @@ func main() {
 func getRewardOffers(w http.ResponseWriter, r *http.Request) {
 	logger.Info("get reward offers")
 	w.Header().Set("Content-Type", "application/json")
+
+	var rewardOffers []RewardOffer
+	rewardOffers, err := FetchRewardOffersFromDataStoreAPI()
+	if err != nil {
+		logger.Error("failed to fetch reward offers", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to fetch reward offers"))
+		return
+	}
+
 	json.NewEncoder(w).Encode(rewardOffers)
 }
 
 func getRewardOffer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
+
+	var rewardOffers []RewardOffer
+	rewardOffers, err := FetchRewardOffersFromDataStoreAPI()
+	if err != nil {
+		logger.Error("failed to fetch reward offers", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to fetch reward offers"))
+		return
+	}
+
 	for _, item := range rewardOffers {
 		if item.Id == params["id"] {
 			json.NewEncoder(w).Encode(item)
@@ -122,8 +132,18 @@ func getRewardOffer(w http.ResponseWriter, r *http.Request) {
 }
 
 func getUserRewards(w http.ResponseWriter, r *http.Request) {
-	logger.Info("get all rewards")
+	logger.Info("get user rewards")
 	w.Header().Set("Content-Type", "application/json")
+
+	var userRewards []UserReward
+	userRewards, err := FetchUserRewardsFromDataStoreAPI()
+	if err != nil {
+		logger.Error("failed to fetch user rewards", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to fetch user rewards"))
+		return
+	}
+
 	json.NewEncoder(w).Encode(userRewards)
 }
 
@@ -132,7 +152,7 @@ func getUserDetails(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
 	var users []User
-	users, err := FetchUsersFromDataStoreApi()
+	users, err := FetchUsersFromDataStoreAPI()
 	if err != nil {
 		logger.Error("failed to fetch users", zap.Error(err))
 		w.WriteHeader(http.StatusInternalServerError)
@@ -153,13 +173,69 @@ func getUserDetails(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&User{})
 }
 
-func FetchUsersFromDataStoreApi() ([]User, error) {
+func FetchRewardOffersFromDataStoreAPI() ([]RewardOffer, error) {
+	// Construct the full URL using the base URL from the environment variable
+	url := fmt.Sprintf("%s/reward-offers", dataStoreApiUrl)
+	// Make the HTTP GET request
+	resp, err := clientCredsConfig.Client(context.Background()).Get(url)
+	if err != nil {
+		logger.Error("failed to fetch reward offers", zap.Error(err))
+		return nil, fmt.Errorf("failed to fetch reward offers: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check for non-200 status codes
+	if resp.StatusCode != http.StatusOK {
+		logger.Warn("API responded with non-200 status code", zap.Int("statusCode", resp.StatusCode))
+		return nil, fmt.Errorf("API responded with status code: %d", resp.StatusCode)
+	}
+
+	// Decode the response body into the User struct
+	var rewardOffers []RewardOffer
+	if err := json.NewDecoder(resp.Body).Decode(&rewardOffers); err != nil {
+		logger.Error("failed to decode reward offers data", zap.Error(err))
+		return nil, fmt.Errorf("failed to decode reward offers data: %v", err)
+	}
+
+	logger.Info("successfully fetched reward offers")
+	return rewardOffers, nil
+}
+
+func FetchUserRewardsFromDataStoreAPI() ([]UserReward, error) {
+	// Construct the full URL using the base URL from the environment variable
+	url := fmt.Sprintf("%s/user-rewards", dataStoreApiUrl)
+	// Make the HTTP GET request
+	resp, err := clientCredsConfig.Client(context.Background()).Get(url)
+	if err != nil {
+		logger.Error("failed to fetch user rewards", zap.Error(err))
+		return nil, fmt.Errorf("failed to fetch user rewards: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check for non-200 status codes
+	if resp.StatusCode != http.StatusOK {
+		logger.Warn("API responded with non-200 status code", zap.Int("statusCode", resp.StatusCode))
+		return nil, fmt.Errorf("API responded with status code: %d", resp.StatusCode)
+	}
+
+	// Decode the response body into the User struct
+	var userRewards []UserReward
+	if err := json.NewDecoder(resp.Body).Decode(&userRewards); err != nil {
+		logger.Error("failed to decode user rewards data", zap.Error(err))
+		return nil, fmt.Errorf("failed to decode user rewards data: %v", err)
+	}
+
+	logger.Info("successfully fetched user rewards")
+	return userRewards, nil
+}
+
+func FetchUsersFromDataStoreAPI() ([]User, error) {
 	// Construct the full URL using the base URL from the environment variable
 	url := fmt.Sprintf("%s/users", dataStoreApiUrl)
 	// Make the HTTP GET request
 	resp, err := clientCredsConfig.Client(context.Background()).Get(url)
 	if err != nil {
-		logger.Error("Failed to fetch users", zap.Error(err))
+		logger.Error("failed to fetch users", zap.Error(err))
 		return nil, fmt.Errorf("failed to fetch users: %v", err)
 	}
 	defer resp.Body.Close()
@@ -173,10 +249,10 @@ func FetchUsersFromDataStoreApi() ([]User, error) {
 	// Decode the response body into the User struct
 	var users []User
 	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
-		logger.Error("Failed to decode users data", zap.Error(err))
+		logger.Error("failed to decode users data", zap.Error(err))
 		return nil, fmt.Errorf("failed to decode users data: %v", err)
 	}
 
-	logger.Info("Successfully fetched users")
+	logger.Info("successfully fetched users")
 	return users, nil
 }
